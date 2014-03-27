cf816b3fb04c8fa624c2a4859fa6f6e9ba52518c
javiapp <jsoliz@pasedfoundation.org>
2014-03-27 16:43:51 -0700

    L10  - Bounce Not Working
    find bug in L10

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -39,14 +39,16 @@ game.BallEntity=me.ObjectEntity.extend({
         settings.spriteheight = "16";
         this.parent(x,y,settings);
         
-        this.setVelocity(2,2);
+        this.setVelocity(10,10);
         this.vel.x += this.accel.x * me.timer.tick;
         this.vel.y += this.accel.y * me.timer.tick;
         
+        this.previousVelocity = this.vel.clone();// record what the veolicity is before hit the wall
+       
         this.collidable = true;
     },
  
-    update:function () {
+    update: function () {
         var collision =  this.collide();// stores object that the ball has collided with; whatever the ball collides with is stored in the variable
         // what do we do if it has a collisoin
         if (collision){ // check if collsion
@@ -55,7 +57,19 @@ game.BallEntity=me.ObjectEntity.extend({
           }//then check what type of collsion that is   
         }
         
-        this.updateMovement();
+        collision = this.updateMovement();  // use to find what the velocity is  [what the velocity is after updating movement IS what's stored into collsion]
+                                                // update movement returns a velocity 
+        if(collision){
+            if(this.vel.x === 0){
+                alert("you hit the side wall");
+                this.vel.x = -this.previousVelocity.x;// if velocity =0 ie it hit the wall then set it back to the neg of previous velocity
+            }
+            if(this.vel.y === 0){
+                this.vel.y = -this.previousVelocity.y;//
+            }
+        }
+        this.previousVelocity = this.vel.clone();// need to again clone the veolicty and store that as the new previous velocity
+        
         return true;
     }
     
